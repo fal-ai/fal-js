1633eb8cd16d89d9ef2308a8d23122805443492f
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-03-31 11:24:17 -0700

    chore(client): add runtime utilities tests

---
+++ libs/client/src/runtime.spec.ts
@@ -0,0 +1,19 @@
+import { getUserAgent, isBrowser } from './runtime';
+
+describe('the runtime test suite', () => {
+  it('should return false when calling isBrowser() on a test', () => {
+    expect(isBrowser()).toBe(false);
+  });
+
+  it('should return true when calling isBrowser() and window is present', () => {
+    global.window = {
+      document: {},
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any;
+    expect(isBrowser()).toBe(true);
+  });
+
+  it('should create the correct user agent identifier', () => {
+    expect(getUserAgent()).toMatch(/@fal\/serverless-client/);
+  });
+});
