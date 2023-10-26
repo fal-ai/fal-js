c0d2c94f3c1790f3924ef4a6b41c91db2143197b
Matteo Ferrando <matteo.ferrando2@gmail.com>
2023-10-26 13:48:16 -0400

    lint

--- libs/client/src/function.ts
+++ libs/client/src/function.ts
@@ -237,7 +237,7 @@ export const queue: Queue = {
   ): Promise<EnqueueResult> {
     return run(id, { ...options, method: 'post', path: '/fal/queue/submit/' });
   },
-  async status(id: string, requestId: string, logs: boolean = false): Promise<QueueStatus> {
+  async status(id: string, requestId: string, logs = false): Promise<QueueStatus> {
     return run(id, {
       method: 'get',
       path: `/fal/queue/requests/${requestId}/status`,
