f520182ed74332ebf48df1f346fb048f78b0840d
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-10-29 23:37:52 -0700

    chore: rename nextjs page router demo

--- apps/demo-nextjs-page-router/jest.config.ts
+++ apps/demo-nextjs-page-router/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'demo-nextjs-app',
+  displayName: 'demo-nextjs-page-router',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/demo-nextjs-app',
+  coverageDirectory: '../../coverage/apps/demo-nextjs-page-router',
 };
