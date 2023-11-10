27b48dade1bd04a93a3f41719bade72687125a0b
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-11-09 17:32:54 -0800

    feat: whisper demo impl

--- apps/demo-nextjs-app-router/app/whisper/page.tsx
+++ apps/demo-nextjs-app-router/app/whisper/page.tsx
@@ -28,38 +28,67 @@ function Error(props: ErrorProps) {
   );
 }
 
-async function record(stream: any): Promise<File> {
-  const audioChunks: any = [];
-  const mediaRecorder = new MediaRecorder(stream);
+type RecorderOptions = {
+  maxDuration?: number;
+};
 
-  mediaRecorder.ondataavailable = (e) => {
-    audioChunks.push(e.data);
-  };
+function useMediaRecorder({ maxDuration = 10000 }: RecorderOptions = {}) {
+  const [isRecording, setIsRecording] = useState(false);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
 
-  return new Promise((resolve, reject) => {
-    mediaRecorder.addEventListener('stop', async () => {
-      console.log('sending request to fal');
-      // Create a blob from the audio chunks
-      const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
-      const audioFile = new File([audioBlob], 'recording.webm', {
-        type: 'audio/webm',
-      });
-      resolve(audioFile);
+  const record = useCallback(async () => {
+    setIsRecording(true);
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const audioChunks: BlobPart[] = [];
+    const recorder = new MediaRecorder(stream);
+    setMediaRecorder(recorder);
+    return new Promise<File>((resolve, reject) => {
+      try {
+        recorder.addEventListener('dataavailable', (event) => {
+          audioChunks.push(event.data);
+        });
+        recorder.addEventListener('stop', async () => {
+          const fileOptions = { type: 'audio/wav' };
+          const audioBlob = new Blob(audioChunks, fileOptions);
+          const audioFile = new File(
+            [audioBlob],
+            `recording_${Date.now()}.wav`,
+            fileOptions
+          );
+          setIsRecording(false);
+          resolve(audioFile);
+        });
+        setTimeout(() => {
+          recorder.stop();
+          recorder.stream.getTracks().forEach((track) => track.stop());
+        }, maxDuration);
+        recorder.start();
+      } catch (error) {
+        reject(error);
+      }
     });
-    setTimeout(() => {
-      console.log('stopped');
-      mediaRecorder.stop();
-    }, 10000);
-    mediaRecorder.start();
-  });
+  }, [maxDuration]);
+
+  const stopRecording = useCallback(() => {
+    setIsRecording(false);
+    mediaRecorder?.stop();
+    mediaRecorder?.stream.getTracks().forEach((track) => track.stop());
+  }, [mediaRecorder]);
+
+  return { record, stopRecording, isRecording };
 }
 
 export default function WhisperDemo() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
   const [logs, setLogs] = useState<string[]>([]);
+  const [audioFile, setAudioFile] = useState<File | null>(null);
   const [result, setResult] = useState<any>(null); // eslint-disable-line @typescript-eslint/no-explicit-any
   const [elapsedTime, setElapsedTime] = useState<number>(0);
+
+  const { record, stopRecording, isRecording } = useMediaRecorder();
 
   const reset = () => {
     setLoading(false);
@@ -69,10 +98,12 @@ export default function WhisperDemo() {
     setResult(null);
   };
 
-  const startRecording = useCallback(async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    return record(stream);
-  }, []);
+  const audioFileLocalUrl = useMemo(() => {
+    if (!audioFile) {
+      return null;
+    }
+    return URL.createObjectURL(audioFile);
+  }, [audioFile]);
 
   const transcribeAudio = async (audioFile: File) => {
     reset();
@@ -109,20 +140,53 @@ export default function WhisperDemo() {
     <div className="min-h-screen dark:bg-gray-900 bg-gray-100">
       <main className="container dark:text-gray-50 text-gray-900 flex flex-col items-center justify-center w-full flex-1 py-10 space-y-8">
         <h1 className="text-4xl font-bold mb-8">
-          Hello <code className="font-light text-pink-600">fal</code> and <code className='text-indigo-500'>Whisper</code>
+          Hello <code className="text-pink-600">fal</code> and{' '}
+          <code className="text-indigo-500">whisper</code>
         </h1>
 
-        <button
-          onClick={async (e) => {
-            e.preventDefault();
-            const audioFile = await startRecording();
-            transcribeAudio(audioFile);
-          }}
-          className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold text-lg py-3 px-6 mx-auto rounded focus:outline-none focus:shadow-outline"
-          disabled={loading}
-        >
-          {loading ? 'Transcribing...' : 'Transcribe'}
-        </button>
+        <div className="flex flex-row space-x-2">
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              try {
+                if (isRecording) {
+                  stopRecording();
+                } else {
+                  const recordedAudio = await record();
+                  setAudioFile(recordedAudio);
+                }
+              } catch (e: any) {
+                setError(e);
+              }
+            }}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold text-lg py-3 px-6 mx-auto rounded focus:outline-none focus:shadow-outline disabled:opacity-70"
+            disabled={loading}
+          >
+            {isRecording ? 'Stop Recording' : 'Record'}
+          </button>
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              if (audioFile) {
+                try {
+                  await transcribeAudio(audioFile);
+                } catch (e: any) {
+                  setError(e);
+                }
+              }
+            }}
+            className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold text-lg py-3 px-6 mx-auto rounded focus:outline-none focus:shadow-outline disabled:opacity-70"
+            disabled={loading || !audioFile}
+          >
+            {loading ? 'Transcribing...' : 'Transcribe'}
+          </button>
+        </div>
+
+        {audioFileLocalUrl && (
+          <div className="text-lg w-full my-2">
+            <audio className="mx-auto" controls src={audioFileLocalUrl} />
+          </div>
+        )}
 
         <Error error={error} />
 
@@ -132,7 +196,7 @@ export default function WhisperDemo() {
             <p className="text-sm text-current/80">
               {`Elapsed Time (seconds): ${(elapsedTime / 1000).toFixed(2)}`}
             </p>
-            <pre className="text-sm bg-black/70 text-white/80 font-mono h-60 rounded whitespace-pre overflow-auto w-full">
+            <pre className="text-sm bg-black/70 text-white/80 font-mono h-96 rounded whitespace-pre overflow-auto w-full">
               {result
                 ? JSON.stringify(result, null, 2)
                 : '// result pending...'}
