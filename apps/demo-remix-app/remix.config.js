841024515b4b41f370cda632dc0743501d49f7b5
Daniel Rochetti <daniel.rochetti@gmail.com>
2023-12-12 09:14:51 -0800

    feat: add remix proxy support

--- apps/demo-remix-app/remix.config.js
+++ apps/demo-remix-app/remix.config.js
@@ -1,5 +1,3 @@
-import { createWatchPaths } from '@nx/remix';
-
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ['**/.*'],
@@ -7,5 +5,4 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
-  watchPaths: () => createWatchPaths('apps/demo-remix-app'),
 };
