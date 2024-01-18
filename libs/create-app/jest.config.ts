8ab374e3a1d351363eb546715a5010bc0d69fb69
Daniel Rochetti <daniel.rochetti@gmail.com>
2024-01-17 19:02:43 -0800

    chore: move folder to match package name

--- libs/create-app/jest.config.ts
+++ libs/create-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'cli',
+  displayName: 'create-app',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/cli',
+  coverageDirectory: '../../coverage/libs/create-app',
 };
