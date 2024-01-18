b76c3714802c2c7a7c285e60d605d030eaee3e06
Nader Dabit <dabit3@gmail.com>
2024-01-17 20:51:54 -0600

    feat: add create app cli (#42)
    * feat: add cli
    
    * chore: removed log
    
    * chore: update repo url
    
    * chore: updated program name
    
    * chore: updated program description
    
    * chore: removed yarn lock files

---
+++ libs/cli/src/index.ts
@@ -0,0 +1,148 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import path from 'path';
+import fs from 'fs';
+import childProcess from 'child_process';
+import ora from 'ora';
+import select from '@inquirer/select';
+import { input } from '@inquirer/prompts';
+import { Command } from 'commander';
+import { execa, execaCommand } from 'execa';
+import open from 'open';
+
+const program = new Command();
+const log = console.log;
+const repoUrl = 'https://github.com/fal-ai/fal-nextjs-template.git';
+const green = chalk.green;
+const purple = chalk.hex('#6e40c9');
+
+async function main() {
+  const spinner = ora({
+    text: 'Creating codebase',
+  });
+  try {
+    const kebabRegez = /^([a-z]+)(-[a-z0-9]+)*$/;
+
+    program
+      .name('Fal.ai App Generator')
+      .description('Generate full stack AI apps integrated with Fal.ai.');
+
+    program.parse(process.argv);
+
+    const args = program.args;
+    let appName = args[0];
+
+    if (!appName || !kebabRegez.test(args[0])) {
+      appName = await input({
+        message: 'Enter your app name',
+        default: 'model-playground',
+        validate: (d) => {
+          if (!kebabRegez.test(d)) {
+            return 'please enter your app name in the format of my-app-name';
+          }
+          return true;
+        },
+      });
+    }
+
+    const hasFalEnv = await select({
+      message: 'Do you have a Fal.ai API key?',
+      choices: [
+        {
+          name: 'Yes',
+          value: true,
+        },
+        {
+          name: 'No',
+          value: false,
+        },
+      ],
+    });
+
+    if (!hasFalEnv) {
+      await open('https://www.fal.ai/dashboard');
+    }
+
+    const fal_api_key = await input({ message: 'Fal AI API Key' });
+
+    let envs = `
+    # environment, either PRODUCTION or DEVELOPMENT
+    ENVIRONMENT="PRODUCTION"
+    
+    # FAL AI API Key
+    FAL_KEY="${fal_api_key}"
+    `;
+
+    log(`\nInitializing project. \n`);
+
+    spinner.start();
+    await execa('git', ['clone', repoUrl, appName]);
+
+    let packageJson = fs.readFileSync(`${appName}/package.json`, 'utf8');
+    const packageObj = JSON.parse(packageJson);
+    packageObj.name = appName;
+    packageJson = JSON.stringify(packageObj, null, 2);
+    fs.writeFileSync(`${appName}/package.json`, packageJson);
+    fs.writeFileSync(`${appName}/.env.local`, envs);
+
+    process.chdir(path.join(process.cwd(), appName));
+    spinner.text = '';
+    let startCommand = '';
+
+    if (isBunInstalled()) {
+      spinner.text = 'Installing dependencies';
+      await execaCommand('bun install').pipeStdout(process.stdout);
+      spinner.text = '';
+      startCommand = 'bun dev';
+      console.log('\n');
+    } else if (isYarnInstalled()) {
+      await execaCommand('yarn').pipeStdout(process.stdout);
+      startCommand = 'yarn dev';
+    } else {
+      spinner.text = 'Installing dependencies';
+      await execa('npm', ['install', '--verbose']).pipeStdout(process.stdout);
+      spinner.text = '';
+      startCommand = 'npm run dev';
+    }
+
+    spinner.stop();
+    process.chdir('../');
+    log(
+      `${green.bold('Success!')} Created ${purple.bold(
+        appName
+      )} at ${process.cwd()} \n`
+    );
+    log(
+      `To get started, change into the new directory and run ${chalk.cyan(
+        startCommand
+      )}\n`
+    );
+  } catch (err) {
+    log('\n');
+    if (err.exitCode == 128) {
+      log('Error: directory already exists.');
+    }
+    spinner.stop();
+  }
+}
+
+main();
+
+function isYarnInstalled() {
+  try {
+    childProcess.execSync('yarn --version');
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function isBunInstalled() {
+  try {
+    childProcess.execSync('bun --version');
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
