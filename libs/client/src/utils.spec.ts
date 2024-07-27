29be2b89add87109417037de3001ea81d54ed200
Daniel Rochetti <daniel.rochetti@gmail.com>
2024-07-27 01:38:52 -0700

    fix: client tests

--- libs/client/src/utils.spec.ts
+++ libs/client/src/utils.spec.ts
@@ -1,11 +1,6 @@
 import { ensureAppIdFormat, parseAppId } from './utils';
 
 describe('The utils test suite', () => {
-  it('shoud match match a legacy appOwner-appId format', () => {
-    const id = '12345-abcde-fgh';
-    expect(ensureAppIdFormat(id)).toBe('12345/abcde-fgh');
-  });
-
   it('shoud match a current appOwner/appId format', () => {
     const id = 'fal-ai/fast-sdxl';
     expect(ensureAppIdFormat(id)).toBe(id);
@@ -19,15 +14,6 @@ describe('The utils test suite', () => {
   it('should throw on an invalid app id format', () => {
     const id = 'just-an-id';
     expect(() => ensureAppIdFormat(id)).toThrowError();
-  });
-
-  it('should parse a legacy app id', () => {
-    const id = '12345-abcde-fgh';
-    const parsed = parseAppId(id);
-    expect(parsed).toEqual({
-      owner: '12345',
-      alias: 'abcde-fgh',
-    });
   });
 
   it('should parse a current app id', () => {
